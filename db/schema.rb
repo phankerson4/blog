06b93f44cb9d9aa765b753d9e95b6defde69636f
phankerson4 <phankerson4@gmail.com>
2020-05-10 23:01:25 -0400

    Set up requests routes/controller view/model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200509031225) do
+ActiveRecord::Schema.define(version: 20200510045715) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
